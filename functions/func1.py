358cbafb9093dc2f4ce681a26e5c685022fb0ec1
anushka <anushka.sj05@gmail.com>
2025-02-14 19:25:27 +0530

    first Commit

---
+++ functions/func1.py
@@ -0,0 +1,17 @@
+# # addition of 3 numbers
+# def add(p,q,r):
+#     return p+q+r
+# p=int(input("Enter 1st no: "))
+# q=int(input("Enter 2nd no: "))
+# r=int(input("Enter 3rd no: "))
+# res=add(p,q,r)
+# print(res)
+# print(add.__doc__)
+def add(p,q,r):
+    return p+q+r
+p=int(input("Enter 1st no: "))
+q=int(input("Enter 2nd no: "))
+r=int(input("Enter 3rd no: "))
+res=add(p,q)
+print(res)
+print(add.__doc__)
