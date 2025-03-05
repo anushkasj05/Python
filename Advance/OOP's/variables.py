4d7250f300a0775a504fcd2311ccbb423d1b6372
anushka <anushka.sj05@gmail.com>
2025-03-05 19:30:26 +0530

    first commit

--- Advance/OOP's/variables.py
+++ Advance/OOP's/variables.py
@@ -1,11 +1,31 @@
-# variables
-# -Static  
-# -instance(object dependent variable )those variables which are changing the value as we change the object
-# -local
-class Students:
-    def __init__(self, name, marks):
-        self.x = name # instance variable
-        self.y = marks # instance variable
-obj=Students("Anushka",5)
-print(obj.x)
-print(obj.y)
+# # variables
+# # -Static  
+# # -instance(object dependent variable)those variables which are changing the value as we change the object
+# # -local
+# class Students:
+#     def __init__(self, name, marks):
+#         self.x = name # instance variable
+#         self.y = marks # instance variable
+#     def show(self):
+#         print(self.x)
+#         print(self.y)
+# obj=Students("Anushka",5)
+# # print(obj.x)
+# # print(obj.y)
+# obj.show()
+# obj1=Students("Divyansh",12)
+# obj1.show()
+# # # can we declare instance variable outside of the class?
+# # # yes we can 
+# # class Students:
+# #     def __init__(self):
+# #         print("display method")
+# #     def show(self):
+# #         print("Value of x ",self.x)
+# # obj=Students()
+# # obj.x=10
+# # obj.show()
+# # with the help of self we can access the variable inside the class
+# # with the help of object we can access the variable outside the class
+####################################################################################################
+# static/class variable(a variable that is not dependent on the object)
