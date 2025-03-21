d5ca64fa2de77613001af70be8e6a78fd12c9e5e
anushka <anushka.sj05@gmail.com>
2025-03-21 19:18:51 +0530

    first commit

---
+++ File Handling/first.py
@@ -0,0 +1,32 @@
+# x => for creating a file 
+# w => for writing in the file as well as creating
+# r => for reading the file (file should already exist in your system)
+# a => for appending
+
+# f=open('n2.py','x')
+# print(f.name)
+# print(f.mode)
+# print(f.readable())
+# print(f.writable())
+# print(f.closed)
+
+# f=open('p2.py','w')
+# print(f.name)
+# print(f.mode)
+# print(f.readable())
+# print(f.writable())
+# print(f.closed)
+
+# f=open('p2.py','r')
+# print(f.name)
+# print(f.mode)
+# print(f.readable())
+# print(f.writable())
+# print(f.closed)
+
+f=open('d2.py','a')
+print(f.name)
+print(f.mode)
+print(f.readable())
+print(f.writable())
+print(f.closed)
